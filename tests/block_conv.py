0e9e0df51fd1fb280ddd2ad6af06c7b78425bab2
anantshah200 <anantshah200@gmail.com>
2019-01-12 01:41:38 +0530

    Small test for type of inputs

--- tests/block_conv.py
+++ tests/block_conv.py
@@ -2,4 +2,24 @@
 # Using the blocksparse module developen by openai
 # Date : 11-1-2019
 
-from 
+from blocksparse.conv import BlocksparseConv, BlocksparseDeconv
+from blocksparse.norms import batch_norm, batch_norm_inference, batch_norm_inf_test, batch_norm_test, batch_norm_grad_test
+import tensorflow as tf
+import numpy as np
+from random import shuffle
+from tensorflow.python.ops import gradient_checker
+
+#Define the input tensor with the given sparsity
+#conv_tensor = BlocksparseConv()
+
+B = 4
+blockC = 32
+blockK = 48
+BCK_diagonal =  [
+                    [
+                        [b*blockC + c for c in range(blockC)],
+                        [b*blockK + k for k in range(blockK)],
+                    ] for b in range(B)
+                ]
+
+print("BCK_diagonal :" + str(BCK_diagonal))
